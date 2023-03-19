d9a61050515c08f9216480694f2fc8aee5b28f6a
L3 Zhang <zhanglebielefeld@gmail.com>
2023-03-19 15:26:43 +0100

    init ok

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # web-dev-intro
 Introduce how a web side works though simple demos
+
+
+host frontend index.html through http-server (install through npm)
+
+```sh
+http-server ./Documents/webDevIntro/ --cors -P http://localhost:3000
+```
---
+++ server.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const http = require('http');
+
+const HOSTNAME = '127.0.0.1';
+const PORT = 3000;
+
+const server = http.createServer();
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+});
+
+server.on('request', (req, res) => {
+  const { headers, method, url } = req;
+
+  const reqUrlArr = url.split('?');
+  const reqQuery = reqUrlArr[1];
+  let reqQueryObj = null;
+  if (reqQuery) {
+    reqQueryObj = parseEqArrToJson(reqQuery);
+    console.log('reqQueryObj: ', reqQueryObj);
+  }
+
+  let data = '';
+  let body = null;
+
+  if (method === 'GET') {
+    data = fs.readFileSync('./counter.txt', {
+      encoding: 'utf-8',
+    });
+    console.log('data: ', data);
+    if (reqQueryObj) {
+      body = JSON.parse(data).filter(v => v.name === reqQueryObj.name);
+    } else {
+      body = null;
+    }
+  }
+
+  res.on('error', (err) => {
+      console.error(err);
+  });
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  const responseBody = { headers, method, url, body };
+  console.log('responseBody: ', responseBody);
+  res.end(JSON.stringify(responseBody));
+})
+
+function parseEqArrToJson(eqArr) {
+  console.log('eqArr: ', eqArr);
+  const obj = {};
+  const arr = eqArr.split('&');
+  arr.forEach(q => {
+    const sp = q.split('=');
+    obj[sp[0]] = sp[0] == 'count' ? +sp[1] : sp[1];
+  });
+  return obj;
+}
